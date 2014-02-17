ae3930e5ecd554bc9404f95cb8ab999d62247b60
Eric Mertens <emertens@gmail.com>
2014-02-17 14:13:04 -0800

    Add NewTTRS.Outcome

---
+++ src/NewTTRS/Outcome.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module NewTTRS.Outcome where
+
+import Control.Lens
+import Data.Monoid
+
+data Outcome = Outcome { _outcomeWins, _outcomeLosses :: !Int }
+  deriving (Eq, Show)
+
+makeLenses ''Outcome
+
+instance Monoid Outcome where
+  mappend (Outcome w1 l1) (Outcome w2 l2) = Outcome (w1+w2) (l1+l2)
+  mempty = Outcome 0 0
+
+-- | Turn wins into losses and losses into wins.
+flipOutcome :: Outcome -> Outcome
+flipOutcome (Outcome a b) = Outcome b a
+
+instance AsEmpty Outcome
