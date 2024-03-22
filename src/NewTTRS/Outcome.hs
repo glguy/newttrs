d93fc21978242f9f12b0380df26b5c98ad053d64
Eric Mertens <emertens@galois.com>
2024-03-22 09:15:27 -0700

    modernize

--- src/NewTTRS/Outcome.hs
+++ src/NewTTRS/Outcome.hs
@@ -11,8 +11,10 @@ data Outcome = Outcome { _outcomeWins, _outcomeLosses :: !Int }
 makeLenses ''Outcome
 
 instance Monoid Outcome where
-  mappend (Outcome w1 l1) (Outcome w2 l2) = Outcome (w1+w2) (l1+l2)
   mempty = Outcome 0 0
+
+instance Semigroup Outcome where
+  Outcome w1 l1 <> Outcome w2 l2 = Outcome (w1+w2) (l1+l2)
 
 -- | Turn wins into losses and losses into wins.
 flipOutcome :: Outcome -> Outcome
